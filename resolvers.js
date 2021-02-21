6d0c169a9973cddb2dc2d1cae6f20b9d755d2d9f
Evgeniya Zhukova <evgeniya.zhukova@georgebrown.ca>
2021-02-20 21:35:23 -0500

    Update Resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,15 @@
+const BookHotel = require('./models/BookHotel');
 const Hotel = require('./models/Hotel');
+const User = require('./models/User');
 
 exports.resolvers = {
     Query: {
+        getBookHotel: async (parent, args) => {
+            return await BookHotel.find({});
+        },
+        getBookHotelByID: async (parent, args) => {
+            return await BookHotel.findById(args.id);
+        },
         getHotel: async (parent, args) => {
             return await Hotel.find({});
         },
@@ -10,9 +18,62 @@ exports.resolvers = {
         },
         getHotelByCity: async (parent, args) => {
             return await Hotel.find({"city" : args.city});
-        }
+        },
+        getUser: async (parent, args) => {
+            return await User.find({});
+        },
+        getUserByID: async (parent, args) => {
+            return await User.findById(args.id);
+        },
     },
     Mutation: {
+        addBookHotel: async (parent, args) => {
+            console.log(args)
+
+            let newBookHotel = new BookHotel({
+                hotel_id: args.hotel_id,
+                booking_date: args.booking_date,
+                booking_start: args.booking_start,
+                booking_end: args.booking_end,
+                user_id: args.user_id,
+            });
+            return await newBookHotel.save();
+        },
+        updateBookHotel: async (parent, args) => {
+            console.log(args)
+            if (!args.id){
+                return;
+            }
+            return await BookHotel.findOneAndUpdate(
+                {
+                    _id: args.id
+                },
+                {
+                    $set: {
+                        hotel_id: args.hotel_id,
+                        booking_date: args.booking_date,
+                        booking_start: args.booking_start,
+                        booking_end: args.booking_end,
+                        user_id: args.user_id
+                    }
+                }, {new: true}, (err, bookhotel) => {
+                    if (err)
+                    {
+                        console.log('Something went wrong when updating the booking of hotel');
+                    } else
+                    {
+                        return bookhotel
+                    }
+                }
+            );
+        },
+        deleteBookHotel: async (parent, args) => {
+            console.log(args)
+            if (!args.id){
+                return JSON.stringify({status: false, "message" : "No ID found"});
+            }
+            return await BookHotel.findByIdAndDelete(args.id)
+        },
         addHotel: async (parent, args) => {
             console.log(args)
             const emailExpression = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
@@ -34,7 +95,7 @@ exports.resolvers = {
         });
         return await newHotel.save();
       },
-      updateHotel: async (parent, args) => {
+        updateHotel: async (parent, args) => {
             console.log(args)
             if (!args.id){
                 return;
@@ -65,12 +126,63 @@ exports.resolvers = {
             }
         );
       },
-      deleteHotel: async (parent, args) => {
+        deleteHotel: async (parent, args) => {
         console.log(args)
         if (!args.id){
             return JSON.stringify({status: false, "message" : "No ID found"});
         }
         return await Hotel.findByIdAndDelete(args.id)
       },
+        addUser: async (parent, args) => {
+            console.log(args)
+            const emailExpression = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            //const isValidEmail =  emailExpression.test(String(email).toLowerCase())
+
+            //if(!isValidEmail){
+            //  throw new Error("email not in proper format")
+            //}
+
+            let newUser = new User({
+                user_id: args.user_id,
+                username: args.username,
+                password: args.password,
+                email: args.email,
+            });
+            return await newUser.save();
+        },
+        updateUser: async (parent, args) => {
+            console.log(args)
+            if (!args.id){
+                return;
+            }
+            return await User.findOneAndUpdate(
+                {
+                    _id: args.id
+                },
+                {
+                    $set: {
+                        user_id: args.user_id,
+                        username: args.username,
+                        password: args.password,
+                        email: args.email
+                    }
+                }, {new: true}, (err, user) => {
+                    if (err)
+                    {
+                        console.log('Something went wrong when updating the user');
+                    } else
+                    {
+                        return user
+                    }
+                }
+            );
+        },
+        deleteUser: async (parent, args) => {
+            console.log(args)
+            if (!args.id){
+                return JSON.stringify({status: false, "message" : "No ID found"});
+            }
+            return await User.findByIdAndDelete(args.id)
+        },
     }
   }
